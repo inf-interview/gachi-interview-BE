9eccb918260c9aba38e3a6c6c45584d2e6fcddff
thstkddnr20 <thstkddnr20@naver.com>
2024-04-03 14:40:37 +0900

    [feat] Post, User 엔티티 등록 strategy = IDENTITY로 하였고 h2데이터베이스 User라는 테이블에 예약어가 있기 때문에 Users로 테이블 명을 교체하였다

--- interview/src/main/java/inflearn/interview/domain/Post.java
+++ interview/src/main/java/inflearn/interview/domain/Post.java
@@ -1,13 +1,32 @@
 package inflearn.interview.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
+@Entity
 public class Post {
-    Long postId;
-    String title;
-    String content;
-    String tag;
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    private String title;
+
+    private String content;
+
+    private String tag;
+
+    private String category;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
--- interview/src/main/java/inflearn/interview/domain/User.java
+++ interview/src/main/java/inflearn/interview/domain/User.java
@@ -1,18 +1,30 @@
 package inflearn.interview.domain;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 
 @Getter
 @Setter
+@Entity
+@Table(name = "Users")
 public class User {
-    Long userId;
-    String name;
-    String email;
-    String social;
-    String createdAt;
-    String updatedAt;
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String name;
+
+    private String email;
+
+    private String social;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
