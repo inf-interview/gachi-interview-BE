c9a6d85340189a06ad9a1e10bb03de4bd86dfc54
thstkddnr20 <thstkddnr20@naver.com>
2024-04-25 15:08:11 +0900

    [update] 재로그인시 email과 social을 확인하도록 수정2 jpql -> spring data jpa 사용

--- interview/src/main/java/inflearn/interview/repository/UserRepository.java
+++ interview/src/main/java/inflearn/interview/repository/UserRepository.java
@@ -9,11 +9,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    @Query("select User from User u where u.email=:email and u.social=:kakao")
-    Optional<User> findKakaoUserByEmail(@Param("email") String email, @Param("kakao") String kakao);
-
-    @Query("select User from User u where u.email=:email and u.social=:google")
-    Optional<User> findGoogleUserByEmail(@Param("email") String email, @Param("google") String google);
+    Optional<User> findUserByEmailAndSocial(String email, String social);
 
 }
