56343bd382ca1455958c8a4fa0f3ff01dc329f9d
kwh <kwh871005@gmail.com>
2024-05-25 20:33:51 +0900

    [feat] VIDEO에 썸네일링크 추가

--- interview/src/main/java/inflearn/interview/controller/InterviewController.java
+++ interview/src/main/java/inflearn/interview/controller/InterviewController.java
@@ -4,10 +4,7 @@ import inflearn.interview.domain.Video;
 import inflearn.interview.repository.VideoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/interview")
@@ -17,7 +14,7 @@ public class InterviewController {
      * 영상 녹화 완료
      */
     private final VideoRepository videoRepository;
-    @GetMapping
+    @PostMapping
     @Transactional
     public void complete(@RequestBody Video video){
         videoRepository.save(video);
