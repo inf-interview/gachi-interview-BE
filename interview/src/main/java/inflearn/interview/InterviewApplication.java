0a177d3ace50bca4c383769896598d5522c3bb8e
thstkddnr20 <thstkddnr20@naver.com>
2024-05-31 15:01:49 +0900

    [chore] fcm 의존성 버전 변경, fcm 초기화 코드 추가

--- interview/src/main/java/inflearn/interview/InterviewApplication.java
+++ interview/src/main/java/inflearn/interview/InterviewApplication.java
@@ -1,9 +1,16 @@
 package inflearn.interview;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 //배포시 exclude부분 삭제
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
@@ -14,4 +21,16 @@ public class InterviewApplication {
 		SpringApplication.run(InterviewApplication.class, args);
 	}
 
+	@PostConstruct
+	public void initFirebase() throws IOException {
+		FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-adminsdk.json");
+
+		FirebaseOptions options = FirebaseOptions.builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.build();
+
+		if (FirebaseApp.getApps().isEmpty()) { // 기본 앱이 초기화되지 않은 경우
+			FirebaseApp.initializeApp(options);
+		}
+	}
 }
