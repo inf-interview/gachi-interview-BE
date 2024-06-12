aeb479a4a01f7dd9ed288def93573df73a289d83
thstkddnr20 <thstkddnr20@naver.com>
2024-06-12 12:39:37 +0900

    [refactor] 기존의 DTO 검증 AOP 수정 기존: @Around를 사용하여 검증에 실패하면 에러코드를 포함한 ResponseEntity 반환 변경 후: @Before을 사용하여 검증 후 검증 실패하면 새로운 예외를 던지고 GlobalExceptionAdvice에서 받아서 처리

--- interview/src/main/java/inflearn/interview/exception/GlobalExceptionAdvice.java
+++ interview/src/main/java/inflearn/interview/exception/GlobalExceptionAdvice.java
@@ -1,10 +1,26 @@
 package inflearn.interview.exception;
 
 
+import inflearn.interview.domain.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UserValidationException.class)
+    public ResponseEntity<ErrorResponse> userValidationFail(UserValidationException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), "User Validation Failed", ex.getMessage(), ex.getPath());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+    }
+
+    @ExceptionHandler(RequestDeniedException.class)
+    public ResponseEntity<ErrorResponse> requestDenied(RequestDeniedException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), "User Validation Failed", "본인의 게시글(댓글)이 아닙니다.");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+    }
 
 }
